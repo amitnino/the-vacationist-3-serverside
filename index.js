cb319f97d86f56412177f9a4f5c183ecd6523438
Amit Nino <amitnino@gmail.com>
2020-12-29 16:02:50 +0200

    changed path of static public folder

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 app.use(cors())
 app.use(express.json())
 
@@ -34,10 +34,10 @@ app.use('/vacations', require('./routes/vacations.js'))
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'public')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 }
 

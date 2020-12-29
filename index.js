91767e85fdfb736ceab4cb1ee9e2487ce738f92d
Amit Nino <amitnino@gmail.com>
2020-12-29 15:30:34 +0200

    fixed port access denied in index.js

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ app.use(express.json())
 app.use('/auth', require('./routes/auth.js'))
 app.use('/vacations', require('./routes/vacations.js'))
 
-app.listen(1000, ()=>console.log(`[SERVER] docked at port 1000`))
+const port = process.env.PORT || 1000
+app.listen(port, err => {
+    if(err) throw err;
+    console.log(`[SERVER] docked at port ${port}`);
+});
